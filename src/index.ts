eb1e8fe0096e4d4111e8847ff9e42b28830e7ea9
sushant <sushantchaudhari5555@gmail.com>
2025-08-08 10:14:26 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import Fastify from 'fastify';
 import {LiveServer} from "./realtime-server";
+import path from "node:path";
+import fastifyStatic from "@fastify/static";
 
 const fastify = Fastify({logger: true});
 
 fastify.register(LiveServer)
+
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'public'),
+    list: true
+})
+
+fastify.get('/admin/test', function (req, reply) {
+    reply.sendFile('test.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+})
 
 fastify.listen({port: Number(process.env.PORT) || 4000, host: '0.0.0.0'}, async (err, address) => {
     if (err) {
